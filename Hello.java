8516982120c52319dceb5b2f0fc4c67e3dc6de14
Jamie Wilkowski <wilkowsk@umich.edu>
2024-02-08 14:21:17 -0500

    Updated on step 1.8

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Step 1.5");
+		System.out.println("Step 1.7");
 		for (int i=1;i<=10;i++) {
 			System.out.println("Hello World..."+i);
 		}
